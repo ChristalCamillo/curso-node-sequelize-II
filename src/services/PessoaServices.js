c71214ee083586414b3a033a1f0bab0d3ceb334a
ChristalCamillo <christal.b.camillo@gmail.com>
2024-02-05 18:10:52 -0300

    implementando transações

--- src/services/PessoaServices.js
+++ src/services/PessoaServices.js
@@ -1,8 +1,11 @@
 const Services = require('./Services.js');
+const dataSource = require('../database/models');
 
 class PessoaServices extends Services {
   constructor() {
     super('Pessoa');
+    //pra alterar coisas na classe de matriculas, vindo o modelo de matricula e todos seus metodos
+    this.matriculaServices = new Services('Matricula');
   }
 
   async pegaMatriculasAtivasPorEstudante(id) {
@@ -21,6 +24,14 @@ class PessoaServices extends Services {
   async pegaPessoasEscopoTodos () {
     const listaPessoas = await super.pegaRegistrosPorEscopo('todosOsRegistros');
     return listaPessoas;
+  }
+
+  async cancelaPessoaEMatriculas(estudante_id){
+    return dataSource.sequelize.transaction(async(transacao) => {
+      await super.atualizaRegistro({ ativo: false}, { id: estudante_id}, transacao);
+      await this.matriculaServices.atualizaRegistro({status: 'cancelado'}, {estudante_id: estudante_id}, transacao);
+    });
+
   }
 }
 
